289d6028a8b81a0f1e5b43625ad2494ff2ad5d77
ElizabethT7 <tymofieievai@gmail.com>
2023-02-16 22:49:04 +0300

    feat: change input-task file

--- src/components/input/Input-task.tsx
+++ src/components/input/Input-task.tsx
@@ -3,7 +3,7 @@ import { Input, Button } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
 import { BoardContent } from '../Constants/constant';
 import styles from './Input.module.scss';
-import Api from '../../api/index';
+import api from '../../api/index';
 
 // import TaskPostInterface from '../Interfaces/Task-post-interface';
 
@@ -13,36 +13,37 @@ interface InputTaskProps {
   setOpen : React.Dispatch<React.SetStateAction<boolean>>,
   type: string,
   listId: number,
-  //AddMoreTask(name: string, id: number): void;
 }
 
 
 export default function InputTask(props: InputTaskProps) {
   const [taskName, setTaskName] = useState('');
   const handleOnChange = (e: React.ChangeEvent) => {
-    console.log((e.target as HTMLTextAreaElement).value)
+    //console.log((e.target as HTMLTextAreaElement).value)
     setTaskName((e.target as HTMLTextAreaElement).value);
   };
 
+  console.log(taskName);
   const id = props.listId;
 
-
+  const [task, setTask] = useState({data: {name: '', list: 0 /*order: 0,*/ }})
   const AddMoreTask = (name: string, id: number) => {
-    const [task, setTask] = useState({data: {name: '', list: 0 /*order: 0,*/ }})
+    
     console.log(name, id);
 
     setTask({data: {name: name, list: id /*order: 0,*/ }});
-    Api.postTask(task);
+    api.postTask(task);
     //Api.getListsAll();
   }
 
-
-
   const handleButtonConfirm = () => {
+    console.log(7, taskName)
     if (props.type === 'task') {
       AddMoreTask(taskName, id);
+      
     }
-    props.setOpen(false)
+    props.setOpen(false);
+    console.log(8)
   }
  return (
     <div>
@@ -56,7 +57,7 @@ export default function InputTask(props: InputTaskProps) {
         />
       </div>
       <div className={styles.buttons__container}>
-      <Button className={props.type === 'task' ? styles.button : styles.button_list} type="primary" onClick={() => handleButtonConfirm} >
+      <Button className={props.type === 'task' ? styles.button : styles.button_list} type="primary" onClick={handleButtonConfirm} >
         {props.type === 'task' ? BoardContent.ADD_TASK : BoardContent.ADD_LIST}
       </Button>
       <Button className={styles.button} icon={<CloseOutlined />} onClick={() => props.setOpen(false)} ></Button>
