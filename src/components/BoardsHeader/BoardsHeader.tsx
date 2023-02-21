710ebc2460b385e5200c0ac15bbfc6fe6aab07fd
OksanaZhuravel <opuseva@mail.ru>
2023-02-21 15:14:30 +0400

    fix: change BoardsHeader

--- src/components/BoardsHeader/BoardsHeader.tsx
+++ src/components/BoardsHeader/BoardsHeader.tsx
@@ -12,33 +12,31 @@ import Member from '../../pages/Workspace/Member';
 import { AppState } from '../../store';
 import { useSelector } from 'react-redux';
 import { WorkspaceContent } from '../Constants/constant';
-import { useBoard } from '../../store/board/hooks';
+// import { useBoard } from '../../store/board/hooks';
+// import board from '../../store/board';
 
 const BoardsHeader: React.FC = () => {
   const [current, setCurrent] = useState('board');
   const userId = useSelector((state: AppState) => state.auth.userId);
-  const $board = useBoard();
-  const board = $board.data;
-  console.log(board);
+  //   const $board = useBoard();
+  //   const board = $board.data;
+  //   console.log(board);
+  //   const userMembers = board.members || [];
+  const userMembers = useSelector(
+    (state: AppState) => state.board.board.members || []
+  );
+  const userBoard = useSelector((state: AppState) => state.board.board || []);
+  console.log(userBoard);
 
-  const userMembers = board.members || [];
-  //   const userMembers = useSelector(
-  //     (state: AppState) => state.board.board.members || []
-  //   );
-  const onClick: MenuProps['onClick'] = (e) => {
-    console.log('click ', e);
-    console.log(userId);
-    setCurrent(e.key);
-  };
   const boardHeaderItems: MenuProps['items'] = [
     {
-      label: board.name,
+      label: userBoard.name,
       key: 'board',
       icon: <EditOutlined />,
     },
     {
       //   label: 'Featured boards',
-      label: board.isFavorite,
+      label: userBoard.isFavorite,
       key: 'featured',
       icon: <StarOutlined />,
     },
@@ -47,20 +45,60 @@ const BoardsHeader: React.FC = () => {
       key: 'Visibility',
       children: [
         {
-          label: board.isClosed,
+          label: userBoard.isClosed,
           //   label: 'Private',
           icon: <LockOutlined />,
-          key: 'Private',
+          key: 'private',
         },
         {
-          label: board.isClosed,
+          //   label: board.isClosed,
           //   label: 'Public',
           icon: <UnlockOutlined />,
-          key: 'Public',
+          key: 'public',
         },
       ],
     },
   ];
+  const onClick: MenuProps['onClick'] = (e) => {
+    // console.log('click ', e);
+    if (e.key === 'board') {
+      console.log(userBoard.name);
+      onChangeBoard(userBoard.name);
+    }
+    if (e.key === 'featured') {
+      console.log(userBoard.isFavorite);
+    }
+    if (e.key === 'private') {
+      console.log(userBoard.isPrivate);
+    }
+    if (e.key === 'public') {
+      console.log(userBoard.isPrivate);
+    }
+    console.log(userId);
+    setCurrent(e.key);
+  };
+
+  const onChangeBoard = async (values: any) => {
+    // const res = await dispatch(
+    //   changeBoard({
+    //     boardId: userBoard.id,
+    //     patch: {
+    //       name: values.name,
+    //       isFavorite: values.isFavorite,
+    //       isPrivate: values.isPrivate,
+    //     },
+    //   })
+    // );
+    console.log('onChangeBoard');
+
+    // const { requestStatus } = res.meta;
+    // if (requestStatus === RequestStatus.FULFILLED) {
+    //   api.success({ message: MeSettingsContent.SETTINGS_SAVED });
+    // }
+    // if (requestStatus === RequestStatus.REJECTED) {
+    //   api.error({ message: MeSettingsContent.SETTINGS_ERROR });
+    // }
+  };
 
   return (
     <>
