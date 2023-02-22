23f299d721df69cec6e3a6953109dec7fe4912eb
ElizabethT7 <tymofieievai@gmail.com>
2023-02-22 16:50:33 +0300

    feat: add edit card button

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,6 +1,8 @@
 import Card from 'antd/es/card/Card';
 import styles from './Task.module.scss';
 import { Task as ITask } from '../../types/task';
+import { Button, Typography } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
 
 interface TaskProps {
   task: ITask;
@@ -11,7 +13,10 @@ export default function Task({ task }: TaskProps) {
   return (
     <div>
       <Card className={styles.task}>
-        {task.name}
+        <div className={styles.task__content}>
+          <Typography className={styles.title}>{task.name}</Typography>
+          <Button className={styles.button_edit} icon={<EditOutlined className={styles.ico}/>}></Button>
+        </div>
       </Card>
     </div>
   )
