7d5a71c1c2ee091edab3de0ec959fa3eeacf0f0d
myspecialspace <myspecialspace@users.noreply.github.com>
2023-02-22 13:13:04 +0300

    fix: workspace settings page

--- src/pages/WorkspaceSettings/WorkspaceSettings.tsx
+++ src/pages/WorkspaceSettings/WorkspaceSettings.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch } from '../../store';
 
 export default function WorkspaceSettingsPage() {
   const dispatch = useAppDispatch();
+  console.log(dispatch);
 
   return <div>WorkspaceSettingsPage</div>
 }
