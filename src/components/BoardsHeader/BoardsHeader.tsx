89fd8fb6f622dc8cb6671e91ebc03a29777e1f50
OksanaZhuravel <opuseva@mail.ru>
2023-02-23 11:31:59 +0400

    refactor: add translation for BoardsHeader

--- src/components/BoardsHeader/BoardsHeader.tsx
+++ src/components/BoardsHeader/BoardsHeader.tsx
@@ -17,7 +17,7 @@ import {
 import Member from '../../pages/Workspace/Member';
 import { AppState, useAppDispatch } from '../../store';
 import { useSelector } from 'react-redux';
-import { BoardContent } from '../Constants/constant';
+import { BoardHeaderContent } from '../Constants/constant';
 import { DeleteBoard } from '../../store/board/types';
 import { useBoard } from '../../store/board/hooks';
 import * as boardThunks from '../../store/board/thunks';
@@ -26,8 +26,8 @@ import * as routerPaths from '../../router/paths';
 
 const BoardsHeader: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [boardName, setBoardName] = useState('');
-  console.log(boardName);
+  const [boardNew, setBoardNew] = useState('');
+  console.log(boardNew);
   const dispatch = useAppDispatch();
   //   const userId = useSelector((state: AppState) => state.auth.userId);
   const $board = useBoard();
@@ -37,7 +37,7 @@ const BoardsHeader: React.FC = () => {
 
   const boardHeaderItems: MenuProps['items'] = [
     {
-      label: `${BoardContent.BOARD_TITLE}${userBoard.name}`,
+      label: `${BoardHeaderContent.BOARD_TITLE}${userBoard.name}`,
       key: 'board',
       //   icon: <EditOutlined />,
     },
@@ -71,32 +71,45 @@ const BoardsHeader: React.FC = () => {
       key: '1',
       label: (
         <Button onClick={() => setIsModalOpen(true)}>
-          {BoardContent.BOARD_SETTING}
+          {BoardHeaderContent.BOARD_SETTING}
         </Button>
       ),
     },
-
     {
       key: '2',
+      label: <Button>{BoardHeaderContent.BOARD_INVITE}</Button>,
+    },
+    {
+      key: '3',
       label: (
-        <Button onClick={deleteBoard}>{BoardContent.DELETE_BOARD_NAME}</Button>
+        <Button onClick={deleteBoard}>
+          {BoardHeaderContent.DELETE_BOARD_NAME}
+        </Button>
       ),
     },
   ];
   const onFinish = (data: any) => {
     console.log('Success:', data);
   };
+  const handleInput = (e: React.ChangeEvent) => {
+    if ((e.target as HTMLTextAreaElement).value.length !== 0) {
+      setBoardNew((e.target as HTMLTextAreaElement).value);
+    }
+  };
+  const handleCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const checked = e.target.checked;
+    console.log(checked);
+  };
   return (
     <>
       <div className={styles.inner}>
         <Menu
           style={{
-            minWidth: 'max-content',
             borderBottom: 'none',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-            width: '250px',
+            minWidth: '250px',
             borderRadius: '10px',
           }}
           mode='horizontal'
@@ -115,7 +128,7 @@ const BoardsHeader: React.FC = () => {
             {userBoard.members.map((member) => (
               <Tooltip
                 key={member.id}
-                title={BoardContent.MEMBERS_TITLE}
+                title={BoardHeaderContent.MEMBERS_TITLE}
                 placement='top'
               >
                 <Member member={member} />
@@ -123,35 +136,45 @@ const BoardsHeader: React.FC = () => {
             ))}
           </Avatar.Group>
         </Space>
-
         <Modal
-          title={BoardContent.UPDATE_BOARD_SETTING}
+          title={BoardHeaderContent.UPDATE_BOARD_SETTING}
           open={isModalOpen}
-          onOk={upDateBoard}
+          //   onOk={upDateBoard}
           onCancel={() => setIsModalOpen(false)}
-          okText='сохранить'
+          okText={BoardHeaderContent.BUTTON_OK}
+          cancelText={BoardHeaderContent.BUTTON_NO}
         >
           <Form name='boardUpdate' onFinish={onFinish}>
-            <Form.Item label={BoardContent.BOARD_TITLE} name='boardName'>
+            <Form.Item label={BoardHeaderContent.BOARD_TITLE} name='boardName'>
               <Input
-                placeholder={BoardContent.UPDATE_BOARD_NAME}
-                onChange={(e) => setBoardName(e.target.value)}
+                placeholder={BoardHeaderContent.UPDATE_BOARD_NAME}
+                //   onChange={(e) => setBoardNew(e.target.value)}
+                onChange={handleInput}
               />
             </Form.Item>
-            <Form.Item
-              name='favorite'
-              valuePropName='checked'
-              wrapperCol={{ offset: 8, span: 16 }}
-            >
-              <Checkbox>Избранное</Checkbox>
-            </Form.Item>
-            <Form.Item
-              name='private'
-              valuePropName='checked'
-              wrapperCol={{ offset: 8, span: 16 }}
-            >
-              <Checkbox>Закрытая</Checkbox>
-            </Form.Item>
+            <Space>
+              <Form.Item
+                name='favorite'
+                valuePropName='checked'
+                wrapperCol={{ offset: 8, span: 16 }}
+              >
+                <Checkbox>{BoardHeaderContent.CHECK_FAVORITE}</Checkbox>
+              </Form.Item>
+              <Form.Item
+                name='private'
+                valuePropName='checked'
+                wrapperCol={{ offset: 8, span: 16 }}
+              >
+                <Checkbox>{BoardHeaderContent.CHECK_PRIVATE}</Checkbox>
+              </Form.Item>
+              <Form.Item
+                name='close'
+                valuePropName='checked'
+                wrapperCol={{ offset: 8, span: 16 }}
+              >
+                <Checkbox>{BoardHeaderContent.CHECK_CLOSE}</Checkbox>
+              </Form.Item>
+            </Space>
           </Form>
         </Modal>
       </div>
